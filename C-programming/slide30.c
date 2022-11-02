deee19980c1fe084d638acb9901bda6584d6631f
Andreas Lundell <andreas.lundell@abo.fi>
2022-11-02 14:48:36 +0000

    add slides 29-39

---
+++ C-programming/slide30.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    char astring[10];
+    int i = 0;
+    
+    scanf( "%s", astring );
+    for ( i = 0; i < 10; ++i )
+    {
+        if ( astring[i] == 'a' )
+        {
+            printf( "You entered an a!\n" );
+        }
+    }
+    getchar();
+}
---
+++ C-programming/slide38_out.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main () {
+   FILE * fp;
+
+   fp = fopen ("file.txt", "w+");
+   fprintf(fp, "%s %s %s %d\n", "We", "are", "in", 2021);
+
+   fclose(fp);
+
+   return(0);
+}
